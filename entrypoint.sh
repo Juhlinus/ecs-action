125099799ff25df9e53ec5d0aaffa99f0678bfee
Linus Juhlin <linus.juhlin@protonmail.com>
2022-07-20 09:46:19 +0900

    Comment out multiple runs so we don't get messed up JSON

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh -l
 set -e
 
-echo "Got these files $1"
-
-OUTPUT=$(/composer/vendor/bin/ecs check $1 --fix)
-OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix)" # Second run just in case
-OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix)" # Third run just in case
+# Run three times to make sure we get everything
+OUTPUT=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
+# OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)"
+# OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)"
 OUTPUT="${OUTPUT//'%'/'%25'}"
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 OUTPUT="${OUTPUT//$'\r'/'%0D'}"
