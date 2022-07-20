82c99b25a5aa2a0a373142863d862268408862e9
Linus Juhlin <linus.juhlin@protonmail.com>
2022-07-20 15:06:17 +0900

    Change from raw output to files instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Run three times to make sure we get everything
-JSON_ONE=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
-JSON_TWO=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
-JSON_THREE=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
-OUTPUT=$(jq -s '.[0] * .[1] * .[2]' "$(echo $JSON_ONE)" "$(echo $JSON_TWO)" "$(echo $JSON_THREE)")
+/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json > json_one.json
+/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json > json_two.json
+/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json > json_thr.json
+OUTPUT=$(jq -s '.[0] * .[1] * .[2]' json_one.json json_two.json json_thr.json)
 
 OUTPUT="${OUTPUT//'%'/'%25'}"
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
