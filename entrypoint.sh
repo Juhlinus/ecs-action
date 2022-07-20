e8a569a40a219cd5588aa618ca1b31da29785f63
Linus Juhlin <linus.juhlin@protonmail.com>
2022-07-20 14:55:39 +0900

    Add JQ back and updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 # Run three times to make sure we get everything
-OUTPUT=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
-# OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)"
-# OUTPUT="$OUTPUT $(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)"
+JSON_ONE=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
+JSON_TWO=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
+JSON_THREE=$(/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json)
+OUTPUT=$(jq -s '.[0] * .[1] * .[2]' $(echo $JSON_ONE) $(echo $JSON_TWO) $(echo $JSON_THREE))
+
 OUTPUT="${OUTPUT//'%'/'%25'}"
 OUTPUT="${OUTPUT//$'\n'/'%0A'}"
 OUTPUT="${OUTPUT//$'\r'/'%0D'}"
