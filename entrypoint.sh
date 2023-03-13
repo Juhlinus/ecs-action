a021adf01dea17dbf4f453a620676765a6cf56f7
Linus Yamashita <linus@yaraku.com>
2023-03-13 15:53:27 +0900

    ZEN-10274 Adjust dockerfile and script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh -l
 set -e
 
-# Run three times to make sure we get everything
-/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json > json_one.json
-/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json > json_two.json
-/composer/vendor/bin/ecs check $1 --fix --clear-cache --output-format=json > json_thr.json
-OUTPUT=$(jq -s '.[0] * .[1] * .[2]' json_one.json json_two.json json_thr.json)
-
-OUTPUT="${OUTPUT//'%'/'%25'}"
-OUTPUT="${OUTPUT//$'\n'/'%0A'}"
-OUTPUT="${OUTPUT//$'\r'/'%0D'}"
+OUTPUT=$(./vendor/bin/pint $1 --test --preset=psr12 -v --format=json)
 
 echo "OUTPUT: $OUTPUT"
+echo "ecs_output=$OUTPUT" >> "$GITHUB_OUTPUT"
+
 echo "::set-output name=ecs_output::$OUTPUT"
